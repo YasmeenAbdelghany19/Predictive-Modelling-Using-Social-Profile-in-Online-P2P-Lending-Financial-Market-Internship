c045932bd21f98feee044e2dbbcacbfdd156f962
agamy5280 <marawan.agamy2010@hotmail.com>
2022-08-05 18:33:04 +0200

    gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,7 +7,8 @@ workers = 4
 client_header_timeout = 3000
 client_body_timeout = 3000
 fastcgi_read_timeout = 3000
-timeout = 3000
-threads = 48
+timeout = 120
+threads = 16
+preload = True
 # Uvicorn's Gunicorn worker class
 worker_class = "uvicorn.workers.UvicornWorker"
